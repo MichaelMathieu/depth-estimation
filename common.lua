caf64abd677d5352d5cb4b24b6bc0578f77494d8
Michael 'myrhev' Mathieu <michael.mathieu@ens.fr>
2012-03-07 17:35:25 -0500

    Created a separate 'common.lua' file for the common funtions

---
+++ common.lua
@@ -0,0 +1,43 @@
+require 'torch'
+
+function randInt(a, b) --a included, b excluded
+   return math.floor(torch.uniform(a, b))
+end
+
+function randomPermutation(n)
+   local ret = torch.Tensor(n)
+   for i = 1,n do
+      local rnd = randInt(1, i+1)
+      ret[i] = ret[rnd]
+      ret[rnd] = i
+   end
+   return ret
+end
+
+-- Get the median of a table.
+function median(t)
+  local temp={}
+
+  -- deep copy table so that when we sort it, the original is unchanged
+  -- also weed out any non numbers
+  for k,v in pairs(t) do
+    if type(v) == 'number' then
+      table.insert( temp, v )
+    end
+ end
+
+  if (#temp == 0) then
+     print("error: median : empty table")
+  end
+
+  table.sort( temp )
+
+  -- If we have an even number of table elements or odd.
+  if math.fmod(#temp,2) == 0 then
+    -- return mean value of middle two elements
+    return ( temp[#temp/2] + temp[(#temp/2)+1] ) / 2
+  else
+    -- return middle elements
+    return temp[math.ceil(#temp/2)]
+  end
+end
--- depth_dataset_median.lua
+++ depth_dataset_median.lua
@@ -4,6 +4,7 @@ require 'sys'
 require 'xlua'
 
 require 'load_data'
+require 'common'
 
 raw_data = {}
 w_imgs = 640
@@ -28,48 +29,6 @@ function getClass(depth)
       return nClasses
    end
    return class
-end
-
-function randInt(a, b) --a included, b excluded
-   return math.floor(torch.uniform(a, b))
-end
-
-function randomPermutation(n)
-   local ret = torch.Tensor(n)
-   for i = 1,n do
-      local rnd = randInt(1, i+1)
-      ret[i] = ret[rnd]
-      ret[rnd] = i
-   end
-   return ret
-end
-
--- Get the median of a table.
-function median(t)
-  local temp={}
-
-  -- deep copy table so that when we sort it, the original is unchanged
-  -- also weed out any non numbers
-  for k,v in pairs(t) do
-    if type(v) == 'number' then
-      table.insert( temp, v )
-    end
- end
-
-  if (#temp == 0) then
-     print("error: median : empty table")
-  end
-
-  table.sort( temp )
-
-  -- If we have an even number of table elements or odd.
-  if math.fmod(#temp,2) == 0 then
-    -- return mean value of middle two elements
-    return ( temp[#temp/2] + temp[(#temp/2)+1] ) / 2
-  else
-    -- return middle elements
-    return temp[math.ceil(#temp/2)]
-  end
 end
 
 function preSortData(wPatch, hPatch, use_median, use_continuous)
