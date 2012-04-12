98da3e0f7077c4ca0acd08d32ba6d9bbe4c8c816
Ricardo Dodds <ric@128-122-140-118.dynapool.nyu.edu>
2012-04-12 19:20:34 -0400

    Depth estimation online test

---
+++ depth_estimation_opticalflow_online.lua
@@ -0,0 +1,88 @@
+require 'torch'
+require 'xlua'
+require 'opticalflow_model'
+require 'openmp'
+require 'sys'
+require 'download_model'
+require 'image_loader'
+
+require 'camera'
+
+torch.manualSeed(1)
+
+op = xlua.OptionParser('%prog [options]')
+-- general
+op:option{'-nt', '--num-threads', action='store', dest='nThreads', default=2,
+	  help='Number of threads used'}
+-- input
+op:option{'-i', '--input-model', action='store', dest='input_model',
+	  help='Trained convnet, this option isn\'t used if -dldir is used'}
+op:option{'-dldir', '--download-dir', action='store', dest='download_dir', default=nil,
+	  help='scp command to the models folder (eg. mfm352@access.cims.nyu.edu:/depth-estimation/models)'}
+op:option{'-rd', '--root-directory', action='store', dest='root_directory',
+	  default='./data', help='Root dataset directory'}
+op:option{'-fi', '--first-image', action='store', dest='first_image', default=0,
+	  help='Index of first image used'}
+op:option{'-d', '--delta', action='store', dest='delta', default=1,
+	  help='Delta between two consecutive frames'}
+-- output
+op:option{'-do', '--display-output', action='store_true', dest='display_output', default=false,
+	  help='Display the computed output'}
+
+opt=op:parse()
+opt.nThreads = tonumber(opt.nThreads)
+opt.first_image = tonumber(opt.first_image)
+opt.delta = tonumber(opt.delta)
+
+openmp.setDefaultNumThreads(opt.nThreads)
+if opt.download_dir ~= nil then
+   opt.input_model = downloadModel(opt.download_dir)
+   if opt.input_model == nil then
+      os.exit(0)
+   end
+end
+
+local loaded = loadModel(opt.input_model, true, true)
+local model = loaded.model
+local filter = loaded.filter
+local geometry = loaded.geometry
+
+local output_window
+local timer
+
+
+local camera = image.Camera{idx=0, width=320, height=180}
+
+-- while true do
+-- 	last_im = camera:forward()
+-- 	d = image.display{image=last_im, win=d, zoom=1}
+-- end
+
+last_im = camera:forward()
+while true do
+   local im = camera:forward()
+   if im then
+       timer = torch.Timer()
+
+	   local input
+	   if geometry.multiscale then
+	      input = {}
+	      for i = 1,#geometry.ratios do
+			input[i] = {last_im[i], im[i]}
+	      end
+	   else
+	      input = prepareInput(geometry, last_im, im)
+	   end
+
+	   local moutput = model:forward(input)
+	   local output = processOutput(geometry, moutput)
+	   print(timer:time())
+	   if opt.display_output then
+	      output_window = image.display{image=output.full, win=output_window}
+	   end
+	   last_im = im
+	   --d = image.display{image=last_im, win=d, zoom=1}
+   end
+end
+
+camera:stop()
