f76d15969daf00cd0b6b698873d7ecf49f7fe099
Michael 'myrhev' Mathieu <michael.mathieu@ens.fr>
2012-04-24 20:13:56 -0400

    Forgot depth_map files

---
+++ ardrone/depth_map.cpp
@@ -0,0 +1,106 @@
+#include "depth_map.h"
+#include "common.h"
+#include "simulator.h"
+#include<cmath>
+#include<ostream>
+#include<iostream>
+using namespace std;
+
+void DepthMap::BinIndex::getPointsInside(size_t nRho, size_t nTheta,
+					 vector<SphericCoordinates> & out) const {
+  float rho1 = getRho1(), rho2 = getRho2();
+  float theta1 = getTheta1(), theta2 = getTheta2();
+  float drho = rho2 - rho1, dtheta = theta2-theta1;
+  const float epsilon = 0.01f;
+  out.resize(nRho*nTheta);
+  for (size_t iRho = 0; iRho < nRho; ++iRho)
+    for (size_t iTheta = 0; iTheta < nTheta; ++iTheta)
+      out[iRho+nRho*iTheta] =
+	SphericCoordinates(map,
+			   rho1+((float)iRho+epsilon)/((float)nRho-1.0f+2*epsilon)*drho,
+			   theta1+((float)iTheta+epsilon)/((float)nTheta-1.0f+2*epsilon)*dtheta);
+}
+
+DepthMap::DepthMap(size_t nBinsRho, size_t nBinsTheta, float maxDepth, float unseenDecay,
+		   float focalLength)
+  :maxDepth(maxDepth), focalLength(focalLength), unseenDecay(unseenDecay),
+   map(nBinsTheta, nBinsRho), theta_sight(0.0f) {
+  assert((nBinsRho > 0) && (nBinsTheta > 0));
+};
+
+DepthMap::BinRay DepthMap::getRayFromPixel(float x, float y, float wImg, float hImg) {
+  float theta = atan2(x-wImg/2, focalLength);
+  return BinRay(this, theta);
+}
+
+void DepthMap::newPixel(float x, float y, float depth, float confidence,
+			float wImg, float hImg) {
+  BinRay ray = getRayFromPixel(x, y, wImg, hImg);
+  size_t iBin = ray.getIBinFromDepth(depth);
+  for (size_t i = 0; i < iBin; ++i)
+    ray[i].value() = 0.5f * (ray[i].value() + 1.0f - confidence);
+  ray[iBin].value() = 0.5f * (ray[iBin].value() + confidence);
+}
+
+void DepthMap::newDisplacement(const matf & pos, const matf & sight) {
+  // position
+  Map new_map(nBinsTheta(), nBinsRho());
+  double tx = pos(0,0), ty = pos(1,0);
+  vector<SphericCoordinates> coords_tmp;
+  for (int iTheta = 0; iTheta < nBinsTheta(); ++iTheta) {
+    for (int iRho = 0; iRho < nBinsRho(); ++iRho) {
+      BinIndex newBin = BinIndex(this, iRho, iTheta);
+      newBin.getPointsInside(5,5, coords_tmp);
+      float value = 0.0f;
+      for (size_t iPt = 0; iPt < coords_tmp.size(); ++iPt) {
+	CartesianCoordinates pt = coords_tmp[iPt].toCartesianCoordinates();
+	pt.add(pos(0,0), pos(1,0));
+	value += pt.toBinIndex().value();
+      }
+      new_map(newBin.iTheta, newBin.iRho) = value/(float)coords_tmp.size()*unseenDecay;
+    }
+  }
+  map = new_map;
+  // angle
+  float theta = atan2(sight(1,0), sight(0,0));
+  if (theta < 0.0f)
+    theta = theta + 2.0f*PI;
+  theta_sight = theta;
+}
+
+void DepthMap::newFrame(matf pixels) {
+  int j = pixels.size().height/2;
+  for (int i = 0; i < pixels.size().width; ++i)
+    newPixel(i, j, pixels(j, i), 1, pixels.size().width, pixels.size().height);
+}
+
+mat3b DepthMap::to2DMap() {
+  int size = 300;
+  float k = 2.0f*maxDepth;
+  mat3b ret(size, size, cv::Vec3b(0.0f, 0.0f, 0.0f));
+  for (int i = 1; i < 5; ++i)
+    ret(150+i, 150)[1] = 255;
+  ret(150, 150)[2] = 255;
+  for (int i = 0; i < size; ++i)
+    for (int j = 0; j < size; ++j) {
+      float x = ((float)i/(float)size-0.5f)*k;
+      float y = ((float)j/(float)size-0.5f)*k;
+      BinIndex bin = CartesianCoordinates(this, x, y).toBinIndex();
+      ret(i, j)[0] = max(ret(i, j)[0], (unsigned char)(255.0f*bin.value()));
+    }
+  return ret;
+}
+
+string DepthMap::toString() {
+  ostringstream oss;
+  char buffer[128];
+  oss << "DepthMap\n";
+  for (int i = 0; i < nBinsRho(); ++i) {
+    for (int j = 0; j < nBinsTheta(); ++j)
+      oss << BinIndex(this, i, j).value() << " ";
+    oss << endl;
+  }
+        
+  oss << "\n";
+  return oss.str();
+}
---
+++ ardrone/depth_map.h
@@ -0,0 +1,104 @@
+#ifndef __DEPTH_MAP_H_20APR2012__
+#define __DEPTH_MAP_H_20APR2012__
+
+#include<vector>
+#include<cstdlib>
+#include "drone_api.h"
+
+class DepthMap {
+public:
+  struct BinIndex;
+  struct SphericCoordinates;
+  struct CartesianCoordinates;
+  struct BinRay;
+  struct Map;
+  
+  struct BinIndex {
+    DepthMap* map;
+    size_t iRho, iTheta;
+    inline BinIndex(DepthMap* map = NULL, size_t iRho = 0, size_t iTheta = 0);
+    inline SphericCoordinates toSphericCoordinates() const;
+    inline CartesianCoordinates toCartesianCoordinates() const;
+    inline float getRho1() const;
+    inline float getRho2() const;
+    inline float getTheta1() const;
+    inline float getTheta2() const;
+    void getPointsInside(size_t nRho, size_t nTheta,
+			 std::vector<SphericCoordinates> & out) const;
+    inline float & value();
+    inline const float & value() const;
+  };
+  struct SphericCoordinates {
+    DepthMap* map;
+    float rho, theta;
+    inline SphericCoordinates(DepthMap* map = NULL, float rho = 0.0f, float theta = 0.0f);
+    inline BinIndex toBinIndex() const;
+    inline CartesianCoordinates toCartesianCoordinates() const;
+  };
+  struct CartesianCoordinates {
+    DepthMap* map;
+    float x, y;
+    inline CartesianCoordinates(DepthMap* map = NULL, float x = 0.0f, float y = 0.0f);
+    inline void add(float x, float y);
+    inline BinIndex toBinIndex() const;
+    inline SphericCoordinates toSphericCoordinates() const;
+  };
+  struct BinRay {
+    DepthMap* map;
+    float theta;
+    inline BinRay(DepthMap* map = NULL, float theta = 0.0f);
+    inline size_t getIBinFromDepth(float depth) const;
+    inline const BinIndex operator[] (size_t i) const;
+    inline BinIndex operator[] (size_t i);
+  };
+  class Map { //vector with smart pointers
+  private:
+    int* nRefs;
+    std::vector<std::vector<float> >* map;
+    size_t d2;
+    inline void decrement();
+  public:
+    inline Map();
+    inline Map(size_t d1, size_t d2, float init = 0.0f);
+    inline Map(const Map & src);
+    inline ~Map();
+    inline Map & operator= (const Map & src);
+    inline size_t size1 () const;
+    inline size_t size2 () const;
+    inline const float & operator() (size_t i, size_t j) const;
+    inline float & operator() (size_t i, size_t j);
+    inline Map clone() const;
+  };
+public:
+  DepthMap(size_t nBinsRho, size_t nBinsTheta, float maxDepth, float unseenDecay,
+	   float focalLength);
+public:
+  // if possible, do not use these functions outside BinIndex, SphericCoordinates and BinRay
+  // (so that it will be easier to add features)
+  inline size_t getIRhoFromRho(float rho) const;
+  inline size_t getIThetaFromTheta(float theta) const;
+  inline float getRhoFromIRho(size_t iRho) const;
+  inline float getRho1FromIRho(size_t iRho) const;
+  inline float getRho2FromIRho(size_t iRho) const;
+  inline float getThetaFromITheta(size_t iTheta) const; //can be > PI (by small amount)
+  inline float getTheta1FromITheta(size_t iTheta) const; //never > PI
+  inline float getTheta2FromITheta(size_t iTheta) const; //can be > PI (by small amount)
+  
+  BinRay getRayFromPixel(float x, float y, float wImg, float hImg);
+  inline size_t nBinsRho () const;
+  inline size_t nBinsTheta () const;
+public:
+  void newPixel(float x, float y, float depth, float confidence, float WImg, float hImg);
+  void newDisplacement(const matf & pos, const matf & sight); 
+  void newFrame(matf pixels);
+  std::string toString();
+  mat3b to2DMap();
+private:
+  float maxDepth, focalLength, unseenDecay;
+  Map map;
+  float theta_sight;
+};
+
+#include "depth_map.hpp"
+
+#endif
